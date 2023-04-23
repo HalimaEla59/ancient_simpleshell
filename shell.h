f3af7ccec6dcb8a0bc278c56da19cece1c035569
HalimaEla59 <halimaelabbadi@gmail.com>
2023-04-23 13:06:52 -0700

    shell.h quick fix

--- shell.h
+++ shell.h
@@ -15,7 +15,7 @@ int my_shell(void);
 char **tokenize(char *cmd);
 char *check_eq(char *str);
 int equals(char *str1, char *str2);
-char **getenvv(char **environ, char *name);
+char *getenvv(char **environ, char *name);
 char **get_path(char **environ);
 
 #endif
